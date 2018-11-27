b1e8bbaa605131dd1de11a0067110937ef75fe7f
John Chappelle III <jchappelleiii@gmail.com>
2018-11-27 10:27:50 -0500

    feat: implement collision detection by way

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ class Enemy {
     this.sprite = 'images/enemy-bug.png';
     this.x = positionX;
     this.y = positionY * 73;
+    this.width = 101;
+    this.height = 80;
     this.speed = speed;
   }
   update(dt) {
@@ -26,6 +28,7 @@ class Enemy {
             // which will ensure the game runs at the same speed for
             // all computers.
         }; */
+
     return this.x >= 505 ? (this.x *= 0) : (this.x += this.speed) * dt;
   }
   // Draw the enemy on the screen, required method for game
@@ -56,12 +59,37 @@ class Player {
     this.sprite = sprite;
     this.initialX = positionX * 101;
     this.initialY = positionY * 80;
+    this.width = 90;
+    this.height = 101;
+  }
+  resetPlayerPos() {
+    this.x = this.initialX;
+    this.y = this.initialY;
+    return;
   }
   update() {
     if (this.y < 68) {
-      this.x = this.initialX;
-      this.y = this.initialY;
-      return;
+      this.resetPlayerPos();
+    }
+    if (collides(player, enemy1)) {
+      console.log('hit!!!!');
+      this.resetPlayerPos();
+    }
+    if (collides(player, enemy2)) {
+      console.log('hit!!!!');
+      this.resetPlayerPos();
+    }
+    if (collides(player, enemy3)) {
+      console.log('hit!!!!');
+      this.resetPlayerPos();
+    }
+    if (collides(player, enemy4)) {
+      console.log('hit!!!!');
+      this.resetPlayerPos();
+    }
+    if (collides(player, enemy5)) {
+      console.log('hit!!!!');
+      this.resetPlayerPos();
     }
   }
   render() {
@@ -84,14 +112,24 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const player = new Player(boy, 2, 5);
+const player = new Player(hornGirl, 2, 5);
 const enemy1 = new Enemy(0, 1, 0.5);
-const enemy2 = new Enemy(0, 1, 1.25);
+const enemy2 = new Enemy(0, 1, 1.7);
 const enemy3 = new Enemy(0, 2, 3);
 const enemy4 = new Enemy(0, 3, 2.45);
-const enemy5 = new Enemy(0, 3, 4.5);
+const enemy5 = new Enemy(0, 3, 3.75);
 allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
 
+// Collision function from https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
+function collides(a, b) {
+  if (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  )
+    return true;
+}
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
